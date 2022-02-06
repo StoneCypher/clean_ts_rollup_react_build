597b4a53e72431d1ddbc6db69be1429ab0ea4990
John Haugeland <stonecypher@gmail.com>
2022-02-05 16:03:56 -0800

    working rollup packaging

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,12 @@
 
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs    from '@rollup/plugin-commonjs';
+import replace     from '@rollup/plugin-replace';
+
+
+
+
+
 const es6_module = {
 
   input     : 'build/typescript/index.js',
@@ -10,7 +18,26 @@ const es6_module = {
     sourcemap : false,
   },
 
-  plugins   : []
+  plugins   : [
+
+    nodeResolve({
+      module         : true,
+      jsnext         : true,
+      browser        : true,
+      extensions     : [ '.js', '.jsx', '.ts', '.tsx' ],
+      preferBuiltins : false
+    }),
+
+    commonjs({
+      include: 'node_modules/**'
+    }),
+
+    replace({
+      'preventAssignment'    : true,
+      'process.env.NODE_ENV' : JSON.stringify( 'production' )
+    })
+
+  ]
 
 };
 
@@ -18,4 +45,80 @@ const es6_module = {
 
 
 
-export default [ es6_module ];
+const iife = {
+
+  input     : 'build/typescript/index.js',
+
+  output    : {
+    file      : 'build/rollup/index.iife.js',
+    format    : 'iife',
+    name      : 'build_ex',
+    sourcemap : false,
+  },
+
+  plugins   : [
+
+    nodeResolve({
+      module         : true,
+      jsnext         : true,
+      browser        : true,
+      extensions     : [ '.js', '.jsx', '.ts', '.tsx' ],
+      preferBuiltins : false
+    }),
+
+    commonjs({
+      include: 'node_modules/**'
+    }),
+
+    replace({
+      'preventAssignment'    : true,
+      'process.env.NODE_ENV' : JSON.stringify( 'production' )
+    })
+
+  ]
+
+};
+
+
+
+
+
+const cjs = {
+
+  input     : 'build/typescript/index.js',
+
+  output    : {
+    file      : 'build/rollup/index.cjs.js',
+    format    : 'commonjs',
+    name      : 'build_ex',
+    sourcemap : false,
+  },
+
+  plugins   : [
+
+    nodeResolve({
+      module         : true,
+      jsnext         : true,
+      browser        : true,
+      extensions     : [ '.js', '.jsx', '.ts', '.tsx' ],
+      preferBuiltins : false
+    }),
+
+    commonjs({
+      include: 'node_modules/**'
+    }),
+
+    replace({
+      'preventAssignment'    : true,
+      'process.env.NODE_ENV' : JSON.stringify( 'production' )
+    })
+
+  ]
+
+};
+
+
+
+
+
+export default [ es6_module, iife, cjs ];
